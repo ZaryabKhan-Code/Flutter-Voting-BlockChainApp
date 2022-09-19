e0eb0ad2b0fb172c89924bc459d0884654663b38
Zaryab Khan <87335498+ZaryabKhan-Code@users.noreply.github.com>
2022-09-19 09:07:05 +0500

    Add files via upload

---
+++ lib/utils/constants.dart
@@ -0,0 +1,10 @@
+String infura_url =
+    "https://kovan.infura.io/v3/be88ecf073be43d89a572fbfd5ee67e7";
+
+String owner_private_key =
+    "4b984aae51aa9f17d30c7ac43fc371165bc756b03860c4222028148530e979f0";
+
+String voter_private_key =
+    "4d7e6b9df7ca048f8b8b4d726602ef72d37efcd7f4a2a42cd27e6726e2532191";
+
+String contractAddress1 = "0xc55a26B3ef93e4Acad3BC4374b54bC3d5F6be3E1";
